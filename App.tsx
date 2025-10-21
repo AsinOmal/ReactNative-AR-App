6021d578bedb9975b306b673f4de0189e88e1fc4
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-10-21 22:33:14 +0530

    A Small 3D Cube was Added

--- App.tsx
+++ App.tsx
@@ -2,17 +2,20 @@ import {
   ViroARScene,
   ViroARSceneNavigator,
   ViroText,
+  ViroBox
 } from "@reactvision/react-viro";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const InitialScene = () => (
   <ViroARScene>
-    <ViroText
-      text={"hello world"}
+    {/* <ViroText
+      text={"Hello"}
       position={[-0.1, 0, -4]}
       style={{ fontSize: 15, fontFamily: "Arial", color: "red" }}
-    />
+    /> */}
+
+    <ViroBox height={2} length={2} width={2} position={[0, -1, -1]} scale={[0.2,0.2,0.2]}/>
   </ViroARScene>
 );
 
