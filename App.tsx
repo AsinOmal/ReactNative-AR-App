9b1cd2ff5fabcd176cb003f3decc0771b63fc727
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-10-21 22:52:39 +0530

    Added a Wooden Texture to the Cube

--- App.tsx
+++ App.tsx
@@ -2,22 +2,40 @@ import {
   ViroARScene,
   ViroARSceneNavigator,
   ViroText,
-  ViroBox
+  ViroBox,
+  ViroMaterials,
+  ViroAnimations,
 } from "@reactvision/react-viro";
 import React from "react";
 import { StyleSheet } from "react-native";
 
-const InitialScene = () => (
-  <ViroARScene>
-    {/* <ViroText
+const InitialScene = () => {
+  ViroMaterials.createMaterials({
+    wood: {
+      diffuseTexture: require("./assets/woodtexture.jpg"),
+    },
+  });
+
+  
+  return (
+    <ViroARScene>
+      {/* <ViroText
       text={"Hello"}
       position={[-0.1, 0, -4]}
       style={{ fontSize: 15, fontFamily: "Arial", color: "red" }}
     /> */}
 
-    <ViroBox height={2} length={2} width={2} position={[0, -1, -1]} scale={[0.2,0.2,0.2]}/>
-  </ViroARScene>
-);
+      <ViroBox
+        height={2}
+        length={2}
+        width={2}
+        materials={["wood"]}
+        position={[0, 0, -1]}
+        scale={[0.2, 0.2, 0.2]}
+      />
+    </ViroARScene>
+  );
+};
 
 export default () => {
   return (
@@ -29,4 +47,3 @@ export default () => {
     />
   );
 };
-
