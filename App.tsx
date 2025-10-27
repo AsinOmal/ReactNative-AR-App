5a6c93ea13c379d25622c39365e2613113182ae1
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2025-10-27 22:12:14 +0530

    Added functionality to switch between 2 Models

--- App.tsx
+++ App.tsx
@@ -7,12 +7,13 @@ import {
   ViroAnimations,
   Viro3DObject,
   ViroAmbientLight,
-  ViroDirectionalLight,
 } from "@reactvision/react-viro";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { StyleSheet } from "react-native";
 
-const InitialScene = () => {
+const InitialScene = (props: any) => {
+  const data = props.sceneNavigator.viroAppProps;
   ViroMaterials.createMaterials({
     wood: {
       diffuseTexture: require("./assets/woodtexture.jpg"),
@@ -33,84 +34,53 @@ const InitialScene = () => {
     },
   });
 
-  // const playstationModel = require("./assets/ps1.obj");
-  // const objResources = [
-  //   require("./assets/playstation-1/source/model/ps1.mtl"), // The Material Library file
-  //   require("./assets/playstation-1/source/model/textures/import-smartlife-no-name.jpg"),
-  //   require("./assets/playstation-1/source/model/textures/jbareham_181120_ply0819_0060.0.jpg"),
-  //   require("./assets/playstation-1/source/model/textures/pes1.png"),
-  //   // You would list all necessary texture files here
-  // ];
-
   return (
     <ViroARScene>
       <ViroAmbientLight color="#ffffff" intensity={1000} />
-
-      {/* <ViroDirectionalLight
-        color="#ffffff"
-        direction={[0, -1, -0.2]}
-        shadowOrthographicPosition={[0, 3, -5]}
-        shadowOrthographicSize={10}
-        castsShadow={true}
-        lightReceivingBitMask={4}
-      /> */}
-
-      <ViroText
-        text={"This is a Cube"}
-        position={[-1, 1, -2]}
-        style={styles.text}
-      />
-
-      {/* <ViroBox
-        height={2}
-        length={2}
-        width={2}
-        materials={["wood"]}
-        position={[0, 0, -1]}
-        animation={{ name: "rotate", loop: true, run: true }}
-        scale={[0.2, 0.2, 0.2]}
-      /> */}
-
-      <ViroBox
-        height={3}
-        length={3}
-        width={3}
-        materials={["metal"]}
-        position={[-1, 0, -2]}
-        animation={{ name: "rotateBox", loop: true, run: true }}
-        scale={[0.2, 0.2, 0.2]}
-      />
-      <ViroAmbientLight color="#FFFFFF" intensity={1000} />
-
-      <Viro3DObject
-        source={require("./assets/porshe911.glb")}
-        type="GLB"
-        position={[0.5, -0.5, -1.5]}
-        scale={[0.2, 0.2, 0.2]}
-        animation={{ name: "rotatex", loop: true, run: true }}
-      />
-
-      {/* <Viro3DObject
-        source={playstationModel}
-        resources={objResources}
-        position={[1, 0, 0]}
-        scale={[0.5, 0.5, 0.5]}
-        type="OBJ"
-        lightReceivingBitMask={4}
-        shadowCastingBitMask={4}
-      /> */}
+      {data.object === "skull" ? (
+        <Viro3DObject
+          source={require("./assets/porshe911.glb")}
+          type="GLB"
+          position={[0.5, -5, -1.5]}
+          scale={[0.2, 0.2, 0.2]}
+          animation={{ name: "rotatex", loop: true, run: true }}
+        />
+      ) : (
+        <ViroBox
+          height={3}
+          length={3}
+          width={3}
+          materials={["metal"]}
+          position={[-1, 0, -2]}
+          animation={{ name: "rotateBox", loop: true, run: true }}
+          scale={[0.2, 0.2, 0.2]}
+        />
+      )}
     </ViroARScene>
   );
 };
 
 export default () => {
+  const [object, setObject] = useState("skull");
   return (
-    <ViroARSceneNavigator
-      initialScene={{
-        scene: InitialScene,
-      }}
-      style={styles.navigator}
-    />
+    <View style={styles.mainView}>
+      <ViroARSceneNavigator
+        initialScene={{
+          scene: InitialScene,
+        }}
+        viroAppProps={{ object: object }}
+        style={styles.navigator}
+      />
+      <View style={styles.controlView}>
+        <TouchableOpacity onPress={() => setObject("skull")}>
+          <Text style={styles.text}>Display Porshe Car</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setObject("tv")}>
+          <Text style={styles.text}>Display Cube</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
@@ -119,8 +89,24 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontFamily: "Arial",
     color: "red",
+    margin: 20,
+    padding: 10,
+    fontWeight: "bold",
+    backgroundColor: "#fff023",
   },
   navigator: {
     flex: 1,
+  },
+  mainView: {
+    flex: 1,
+  },
+  controlView: {
+    width: "100%",
+    height: 100,
+    borderRadius: 23,
+    backgroundColor: "#ffffff",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
